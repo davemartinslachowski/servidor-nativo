a65c4621654670d4f236a1c62e1d0c1e4243941b
MartinsClv <drakesdavi@gmail.com>
2026-08-18 02:25:09 +0000

    Exercicio 3

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const http = require('node:http');
 const porta = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(`Requisição recebida! ${req.method} ${req.url}`);
-  res.statusCode = 201;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('Recurso criado');
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    console.log(`Data e hora: ${new Date().toISOString()}`);
+
+    res.statusCode = 201;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ status: "ok" }));
 });
 
 server.listen(porta, () => {
-  console.log(`Servidor ouvindo na porta ${porta}`);
+    console.log(`Servidor ouvindo na porta ${porta}`);
 });
